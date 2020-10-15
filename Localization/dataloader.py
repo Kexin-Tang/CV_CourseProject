67c2f0a6263e32383a1f478d2506769b9fdb9922
渴昕 <68990238+Kexin-Tang@users.noreply.github.com>
2020-10-15 15:25:42 +0800

    Update dataloader.py

--- Localization/dataloader.py
+++ Localization/dataloader.py
@@ -13,12 +13,15 @@ def readData(rote, className):
     imgs = []                                       # img's pixels
     img_file = os.path.join(rote, className)        # enter folder
     for file in os.listdir(img_file):
-        img = cv2.imread(img_file + '\\' + file)    # read imgs(only first 180 imgs are valid, will process later)
+        img = cv2.imread(img_file + '/' + file)     # read imgs(only first 180 imgs are valid, will process later)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # torch is RGB, opencv is BGR, need transfer
+        img = np.array(img/255, dtype=np.float)
         imgs.append(img)
 
     gt_file = os.path.join(rote, className+"_gt.txt")   # ground truth txt file
     gts = np.loadtxt(gt_file)                           # get ground truth
     gts = gts[:, 1:]                                    # 1st column is index, which is redundant
+    gts = np.array(gts)
     return imgs, gts
 
 '''
@@ -31,37 +34,57 @@ def readData(rote, className):
 def process(class_list, rote):
     trainData = []
     trainGT = []
+    trainLabel = []
     testData = []
     testGT = []
+    testLabel = []
+
+    i = 0
 
     for className in class_list:
-        print('Loading ' + className + ' ...')
         imgs, gts = readData(rote, className)
         # first 150 imgs for training, 150~180 for testing
         trainData.append(imgs[:150])
         trainGT.append(gts[:150])
+        trainLabel.append(np.array([i] * 150))
+
         testData.append(imgs[150:180])
         testGT.append(gts[150:180])
-        print('Loading ' + className + 'finished!')
+        testLabel.append(np.array([i] * 30))
+        i+=1
+
     trainData = np.array(trainData)
     trainGT = np.array(trainGT)
+    trainLabel = np.array(trainLabel)
     testData = np.array(testData)
     testGT = np.array(testGT)
-    trainData = trainData.reshape(-1, 128, 128, 3)
+    testLabel = np.array(testLabel)
+
+    trainData = trainData.reshape(-1, 128, 128, 3)  # [5, 150, 128, 128, 3] -> [750, 128, 128, 3]
     trainGT = trainGT.reshape(-1, 4)
-    testData = testData.reshape(-1, 128, 128, 3)
+    trainLabel = trainLabel.reshape(-1)
+
+    testData = testData.reshape(-1, 128, 128, 3)    # [5, 30, 128, 128, 3] -> [150, 128, 128, 3]
     testGT = testGT.reshape(-1, 4)
-    return trainData, trainGT, testData, testGT
+    testLabel = testLabel.reshape(-1)
+
+    # very important! transfer numpy shape to tensor shape [batch, H, W, C] -> [batch, C, H, W]
+    trainData = np.transpose(trainData, (0, 3, 1, 2))
+    testData = np.transpose(testData, (0, 3, 1, 2))
+
+    return trainData, trainGT, trainLabel, testData, testGT, testLabel
 
 
 
 if __name__ == "__main__":
-    ''' please remeber to change these params to your own dataset and absolute path '''
     class_list = ['bird', 'car', 'dog', 'lizard', 'turtle']     # define the 5 classes
-    rote = "XXX" # define the rote path
+    rote = "/home/kxtang/CV/tiny_vid" # define the rote path
 
-    trainData, trainGT, testData, testGT = process(class_list, rote)
-    print(trainData.shape)  # (150*classNum, 128, 128, 3)
+    trainData, trainGT, trainLabel, testData, testGT, testLabel = process(class_list, rote)
+    print(trainData.shape)  # (150*classNum, 3, 128, 128)
     print(trainGT.shape)    # (150*classNum, 4)
-    print(testData.shape)   # (30 *classNum, 128, 128, 3)
+    print(trainLabel.shape) # (150*classNum, )
+    print(testData.shape)   # (30 *classNum, 3, 128, 128)
     print(testGT.shape)     # (30 *classNum, 4)
+    print(testLabel.shape)  # (30*classNum, )
+
